661f33ce33a80615047b4c0f95873766f020c3b8
Anusri Chavali <chavali.anusri@gmail.com>
2024-04-27 09:21:42 -0700

    adding Erica's character

--- Starting_Page/character_selection.py
+++ Starting_Page/character_selection.py
@@ -25,9 +25,11 @@ pygame.display.flip()
 title_font = pygame.font.Font("resources/Grand9KPixel.ttf", 60)
 subtitle_font = pygame.font.Font("resources/Grand9KPixel.ttf", 40)
 
+segment_width = screen_info.current_w/4
 #use button_creation function to create selecot buttons for both characters and next button
-choose_neha = button_creation("Choose Neha", (screen_info.current_w/3 - 100), 2 * (screen_info.current_h/3))
-choose_anusri = button_creation("Choose Anusri", 2* (screen_info.current_w/3) - 100, 2 * (screen_info.current_h/3))
+choose_neha = button_creation("Choose Neha", (segment_width - 75), 2 * (screen_info.current_h/3))
+choose_anusri = button_creation("Choose Anusri", 2* (segment_width) - 75, 2 * (screen_info.current_h/3))
+choose_erica = button_creation("Choose Erica", 3 * (segment_width) - 75, 2 * (screen_info.current_h/3))
 next_button = button_creation("Next", screen_info.current_w - 300, screen_info.current_h - 200)
 
 #load sprite images and resize them
@@ -37,6 +39,9 @@ anusri_img = pygame.transform.scale(anusri_img, DEFAULT_SIZE)
 
 neha_img = pygame.image.load("resources/Neha_Sprite.png")
 neha_img = pygame.transform.scale(neha_img, DEFAULT_SIZE)
+
+erica_img = pygame.image.load("resources/Erica_Sprite.png")
+erica_img = pygame.transform.scale(erica_img, DEFAULT_SIZE)
 
 
 #initialize variables before running the character_selection page
@@ -57,14 +62,22 @@ def run():
                 if choose_neha[3].collidepoint(mouse_position):
                     choose_neha[0].fill("#f77a5e")
                     choose_anusri[0].fill("#f2461f")
+                    choose_erica[0].fill("#f2461f")
                     character_selected = "Neha"
                     print("Neha")
                 #store "anusri" as selected character and configure button colors to show selection
                 elif choose_anusri[3].collidepoint(mouse_position):
                     choose_anusri[0].fill("#f77a5e")
                     choose_neha[0].fill("#f2461f")
+                    choose_erica[0].fill("#f2461f")
                     character_selected = "Anusri"
                     print("Anusri")
+                elif choose_erica[3].collidepoint(mouse_position):
+                    choose_erica[0].fill("#f77a5e")
+                    choose_anusri[0].fill("#f2461f")
+                    choose_neha[0].fill("#f2461f")
+                    character_selected = "Erica"
+                    print("Erica")
                 #create or open the existing "selected_character.txt" file and write the finally selected character to the file
                 #move to the next page
                 elif next_button[3].collidepoint(mouse_position) and character_selected != "":
@@ -82,14 +95,16 @@ def run():
         # while the program is running, display the Select character buttons
         choose_neha[0].blit(choose_neha[1], choose_neha[2])
         choose_anusri[0].blit(choose_anusri[1], choose_anusri[2])
+        choose_erica[0].blit(choose_erica[1], choose_erica[2])
         screen.blit(choose_neha[0], (choose_neha[3].x, choose_neha[3].y))
         screen.blit(choose_anusri[0], (choose_anusri[3].x, choose_anusri[3].y))
+        screen.blit(choose_erica[0], (choose_erica[3].x, choose_erica[3].y))
 
         #while the program is running, display the character images on the screen
         screen.blit(anusri_img, (choose_anusri[3].x - 50, choose_anusri[3].y - anusri_img.get_height() - 50))
         screen.blit(neha_img, (choose_neha[3].x - 50, choose_neha[3].y - neha_img.get_height() - 50))
+        screen.blit(erica_img, (choose_erica[3].x - 50, choose_erica[3].y - erica_img.get_height() - 50))
 
-        #screen.blit(background_img, (0, screen_info.current_h/2))
 
         #display the next button only after one of the characters has been selected
         if character_selected != "":
