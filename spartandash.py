5968c6bfb9f292f8ecf0d6564d6f345c771eda6d
Neha Washikar <neha.washikar@gmail.com>
2024-05-01 18:18:01 -0700

    added assets and logic to read char/loc

--- spartandash.py
+++ spartandash.py
@@ -21,7 +21,15 @@ plane_shooter = pygame.image.load("resources/shooter-green.png")
 sprite_shooter = pygame.image.load("resources/shooter-sprite.png")
 
 #load sprite (main player)
-sprite = pygame.image.load("resources/Neha_Sprite.png")
+with open('Starting_Page/selected_character.txt', 'r') as file:
+    # Read the first line
+    character = file.readline().strip()
+if(character == "Neha"):
+    sprite = pygame.image.load("resources/Neha_Sprite.png")
+elif(character == "Anusri"):
+    sprite = pygame.image.load("resources/Anusri_Sprite.png")
+elif(character == "Erica"):
+    sprite = pygame.image.load("resources/Erica_Sprite.png")
 
 #create the screen and configure the screen size and caption
 screen_info = pygame.display.Info()
@@ -31,13 +39,36 @@ pygame.display.set_caption("SpartanDash")
 pygame.display.flip()
 
 #load and scale background
+with open('Starting_Page/selected_location.txt', 'r') as file:
+    location = file.readline().strip()
+
+if(location == "SRAC"): #CHANGE THIS TO TOWER LAWN!!!
+    background = pygame.image.load("resources/tower_lawn.jpeg")
+elif(location == "SRAC"):
+    sprite = pygame.image.load("resources/Anusri_Sprite.png")
+elif(location == "MLK"):
+    sprite = pygame.image.load("resources/Erica_Sprite.png")
+
+
 DEFAULT_SIZE = (350, 245)
-background = pygame.image.load("resources/tower_lawn.png")
+#background = pygame.image.load("resources/tower_lawn.jpeg")
 original_width, original_height = background.get_size()
 new_height = int((screen_info.current_w / original_width) * original_height)
 background = pygame.transform.scale(background, (screen_info.current_w, new_height))
 
 font = pygame.font.Font("resources/Grand9KPixel.ttf", 25)
+
+class Asset:
+    def __init__(self, xPos, yPos, health = 100):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.health = health
+        self.sprite_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), (self.xPos, self.yPos, 50, 50))
 
 #main loop
 def run():
@@ -47,16 +78,19 @@ def run():
     level = 1
     lives = 5
 
+    asset = Asset(300, 650)
+    #asset.draw(screen)
+
     while running:
         timer.tick(FPS)
-        set_background(level, lives)
+        set_background(level, lives, asset)
 
         for event in pygame.event.get():
             #exit the loop and close the screen in player quits
             if event.type == pygame.QUIT:
                 running = False
 
-def set_background(level, lives):
+def set_background(level, lives, asset):
     #draw background to screen
     screen.blit(background, (0,0)) 
     pygame.display.update()
@@ -66,5 +100,9 @@ def set_background(level, lives):
 
     screen.blit(levels_text, (10, 10))
     screen.blit(lives_text, (10, 50))
+
+    asset.draw(screen)
+
+    pygame.display.update()
 
 run()     
